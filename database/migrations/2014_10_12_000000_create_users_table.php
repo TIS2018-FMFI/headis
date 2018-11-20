31fee491425a06fc41354b73c842220daa6db86a
Tomas Velich <tomas@digitalwerk.agency>
2018-11-20 14:44:45 +0100

    [TASK] user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('user_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('level');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('isRedactor')->default(false);
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
