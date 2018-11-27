beb53f17c5d34bdc80f8f40388a15ef97dd0d17e
rychtovy24 <rychtovy@gmail.com>
2018-11-27 13:52:15 +0100

    [TASK] Users migrations update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('level');
+            $table->integer('position');
             $table->string('first_name');
             $table->string('last_name');
             $table->boolean('isRedactor')->default(false);
