a6e4e296ba979a731ee9621c591499b85c49228f
rychtovy24 <rychtovy@gmail.com>
2018-11-27 23:36:55 +0100

    [TASK] Generate users.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,11 +14,15 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
+    static $position = 1;
     return [
-        'name' => $faker->name,
+        'user_name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'position' => $position++,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->delete();
+        DB::table('users')->insert(array(
+            'user_name' => 'matej24',
+            'email' => 'test@test.sk',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'remember_token' => str_random(10),
+            'position' => 0,
+            'first_name' => 'Matej',
+            'last_name' => 'Rychtarik',
+            'isRedactor' => true,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ));
+        factory(App\User::class, 10)->create();
     }
 }
