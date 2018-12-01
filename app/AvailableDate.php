febdce38780af928ecf4e025a2418405a3ab9079
Tomas Velich <tomas@digitalwerk.agency>
2018-12-01 14:30:38 +0100

    [TASK] models timestamp not use

--- app/AvailableDate.php
+++ app/AvailableDate.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class AvailableDate extends Model
 {
-    //
+    public $timestamps = false;
 }
--- app/Challenge.php
+++ app/Challenge.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Challenge extends Model
 {
-    //
+    public $timestamps = false;
 }
