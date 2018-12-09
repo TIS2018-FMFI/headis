942ece1bdcd73750b507f7bfe6d4d6e5a17dc34f
Tomas Velich <tomas@digitalwerk.agency>
2018-12-09 11:34:34 +0100

    [TASK] enable post list for guest

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('index');
 
         $this->middleware('redactor')->except('index');
     }
