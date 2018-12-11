469577ab68bbb4e284354e895a02dee62f6cd378
Tomas Velich <tomas@digitalwerk.agency>
2018-12-11 14:25:24 +0100

    [TASK] global user for every view

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +17,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        view()->composer('*', function ($view) {
+            $user = null;
+            if (auth()->check()) {
+                $user = User::find(auth()->user()->id);
+            }
+            $view->with('currentAuthUser', $user);
+        });
     }
 
     /**
