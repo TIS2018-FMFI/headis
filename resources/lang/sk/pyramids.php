43daa491855ebdb87a27b989611bea04d2dbd8d4
rychtovy24 <rychtovy@gmail.com>
2019-01-11 10:18:05 +0100

    [TASK] langueges for pyramid and user

---
+++ resources/lang/sk/pyramids.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pyramid Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'Date of start season' => 'Začiatok sezóny',
+    'Date of end season' => 'Koniec sezóny',
+    'The best player' => 'Najlepší hráč'
+
+];
---
+++ resources/lang/sk/users.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | User Language Lines
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+
+    'First name' => 'Meno',
+    'Last name' => 'Priezvisko',
+    'Opponent' => 'Súper',
+    'Challenge date' => 'Dátum zápasu',
+    'Set' => 'Set',
+    'Winner' => 'Výherca'
+
+];
