19f88838ac681dec9dc6d65c8f163eb8fcd7d97d
Marián Bagyanszký <bagyanszky1@uniba.sk>
2019-01-23 10:29:13 +0100

    [TASK] update match showblade

--- resources/views/match/show.blade.php
+++ resources/views/match/show.blade.php
@@ -5,7 +5,6 @@
     <match-component :match="{{ json_encode($match) }}"
                      :finished="{{ json_encode($finished) }}"
                      :current_user="{{ json_encode($currentAuthUser) }}"
-                     :confirmed=" {{ json_encode($confirmed) }} "
 
     ></match-component>
 
