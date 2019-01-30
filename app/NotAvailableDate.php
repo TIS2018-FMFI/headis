54e8486e5f88eaabb2f7fac0cd3573e4b41bfd8a
rychtovy24 <rychtovy@gmail.com>
2019-01-30 09:57:25 +0100

    [TASK] is Available date function

--- app/NotAvailableDate.php
+++ app/NotAvailableDate.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class NotAvailableDate extends Model
@@ -11,5 +12,10 @@ class NotAvailableDate extends Model
     public function season()
     {
         return $this->belongsTo(Season::class);
+    }
+
+    public static function isAvailableDate()
+    {
+        return NotAvailableDate::whereDate('date', '=', Carbon::today())->get() == null;
     }
 }
