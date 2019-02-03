2a27bd42a40369835001ff798cd7c60ba72ac9a0
Tomas Velich <tomas@digitalwerk.agency>
2019-02-03 12:46:49 +0100

    [TASK] fix post

--- app/Post.php
+++ app/Post.php
@@ -23,7 +23,7 @@ class Post extends Model
 
     public static function allAvailable()
     {
-        if (auth()->user()->isRedactor) {
+        if (auth()->user() && auth()->user()->isRedactor) {
             return static::orderByDesc('created_at')->paginate(9);
         }
         return static::where('hidden', false)->orderByDesc('created_at')->paginate(9);
